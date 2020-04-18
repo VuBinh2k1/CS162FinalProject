a7e3acfc5830c660b714297abf8dc46d487a64e0
VuBinh <binh161905@gmail.com>
2020-04-18 21:14:40 +0700

    commit layout (make for fun)

--- layout/layout.h
+++ layout/layout.h
@@ -5,6 +5,8 @@
 #define green_code 10
 #define red_code 4
 #define white_code 15
+#define MAX_WIDTH = 100
+#define MAX_HEIGHT = 30
 
 void colorizing(WORD color_code);
 void gotoxy(int column, int line, WORD color_code = default_code);
