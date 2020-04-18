c27edcd4c6be27b29f9a6313059bada9db0e7bc7
VuBinh <binh161905@gmail.com>
2020-04-19 01:51:54 +0700

    commit login layout

--- layout/layout.h
+++ layout/layout.h
@@ -1,5 +1,9 @@
 #pragma one
+#pragma warning(disable : 4996)
 #include <Windows.h>
+#include <iostream>
+#include <fstream>
+#include <utility>
 #define default_code 7
 #define blue_code 1
 #define green_code 10
@@ -8,5 +12,26 @@
 #define MAX_WIDTH = 100
 #define MAX_HEIGHT = 30
 
+typedef struct mess {
+	int xo, yo, height, width, color_code;
+	char** data;
+
+	void init(std::istream& inp);
+	void realease();
+	void print();
+}mess;
+
+typedef struct layout {
+	int image_count, input_count;
+	mess* data;
+	std::pair <int, int>* in;
+
+	layout(std::istream& inp);
+	~layout();
+	void print();
+}layout;
+
 void colorizing(WORD color_code);
 void gotoxy(int column, int line, WORD color_code = default_code);
+
+void print_login_layout(char*& user, char*& pass, bool has_invalid = 0);
--- source.cpp
+++ source.cpp
@@ -5,7 +5,9 @@
 using std::cin;
 using std::cout;
 
-int main(int argc, char* agrv[])
-{
+int main(int argc, char* agrv[]) {
+    char* a, * b;
+    print_login_layout(a, b);
+    while (strcmp(a, "vubinh"))print_login_layout(a, b, 1);
     return 0;
 }
