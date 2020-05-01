8b10a0b3992712cefeefb1d932e4a3bd1a1bd6a8
ltkhoi <lethanhkhoi081001@gmail.com>
2020-05-01 12:16:42 +0700

    Update class.cpp

--- routes/class.cpp
+++ routes/class.cpp
@@ -103,10 +103,40 @@ void npclass::studentlist()
 	minibox_layout.print();
 	std::string class1;
 	gotoxy(33, 9);
-	std::cout << "Input the class you want to view: "; std::cin.ignore(); getline(std::cin, class1);
+	std::cout << "Input the class you want to view: "; std::cin.ignore('\0'); getline(std::cin, class1);
 	std::transform(class1.begin(),class1.end(),class1.begin(),::toupper);	
 	std::string filename;
-	filename = (std::string)(".\\data\\class\\" + class1 + ".csv");
-	gotoxy(33, 9, COLOR_YELLOW_BACKGROUND); std::cout << "    Status     |       ID                        ";
+	filename = (std::string)(".\\data\\class\\") + class1 + ".csv";
+	gotoxy(33, 9, COLOR_YELLOW_BACKGROUND); std::cout << " No.   |    Status     |      ID                       ";
 	csv_file file1(filename.c_str());
+	int choose = 0;
+	while (1)
+	{
+		int j = 0;
+		for (int i = choose; i < min(choose + 18, file1.count);++i)
+		{
+			int y = 10 + j - choose;
+			WORD COLOR_CODE = (i % 2) ? 112 : 240;
+			csv_line* student = &file1.data[i];
+			if (atoi(file1.data[i].pdata[0]) == 1)
+			{
+				gotoxy(33, y, COLOR_CODE); std::cout << "       |               |                               ";
+				gotoxy(34, y, COLOR_CODE); std::cout << j;
+				gotoxy(43, y, COLOR_CODE); std::cout << file1.data[i].pdata[0];
+				gotoxy(57, y, COLOR_CODE); std::cout << file1.data[i].pdata[1];
+				j++;
+			}
+		}
+	NO_CHANGE:
+		uint8_t c = getch();
+		if (c == KEY_ESC) break;
+		if (c == KEY_ENTER);
+		if (c == 224 || c == 0) {
+			c = getch();
+			if (c == KEY_UP && choose > 0) choose--;
+			else if (c == KEY_DOWN && choose < file1.count - 18) choose++;
+			else if (c == KEY_LEFT) break;
+			else goto NO_CHANGE;
+		}
+	}
 }
