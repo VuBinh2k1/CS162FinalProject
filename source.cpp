72711e7b1e63425a47a88de7093eb54f6af609b9
19127012 <63223684+VuBinh2k1@users.noreply.github.com>
2020-05-25 21:16:46 +0700

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -5,9 +5,8 @@ int main(int argc, char* agrv[]) {
     SetConsoleTitle(TEXT("Student management system"));
     csv_line user;
     while (role::login(user)) {
-		//loading();
-
-		if (user == "staff") role::staff(user);
+	//loading(1, 18, 98);
+        if (user == "staff") role::staff(user);
         if (user == "lecturer") role::lecturer(user);
         if (user == "student") role::student(user);
     }
